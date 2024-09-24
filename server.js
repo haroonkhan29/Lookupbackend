4ba4907ac2b7b31c7cf73d47c0e75147c498cca6
haroonkhan29 <haroon77.afridi@gmail.com>
2024-09-24 15:26:15 +0500

    port change

--- server.js
+++ server.js
@@ -16,6 +16,6 @@ app.use(cors());
 
 app.use('/api/auth', require('./routes/authRoutes'));
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
